7d5d1f694689b76d240170f6ba91103990ec92ad
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-04 17:57:54 +0530

    feat: add CORS support to enable cross-origin requests

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,10 +3,17 @@ const app = express();
 const authRouter = require('./routes/auth.routes.js');
 const uploadRouter = require('./routes/upload.routes.js');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/auth', authRouter);
